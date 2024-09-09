30ae16fb2ebdf50cbf8f9554ea55cd78aa0422e4
unknown <danil.perfiliv@gmail.com>
2024-09-09 07:34:53 -0700

    use effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import QRCodeScanner from './components/QRCodeScanner';
 import ARModel from './components/ARModel';
 
